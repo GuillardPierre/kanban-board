294974c58a4b74ba248f2577647d97c6f55461b8
Pierre Guillard <pguillard95@gmail.com>
2025-03-10 17:18:21 +0100

    add dragAndDrop functionnality

---
+++ dragAndDrop.js
@@ -0,0 +1,41 @@
+const allCards = document.querySelectorAll('.card');
+const columns = document.querySelectorAll('.column');
+
+let dragged = null;
+
+columns.forEach((column) => {
+  column.classList.add('dropzone');
+
+  column.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+
+  column.addEventListener('dragenter', (event) => {
+    event.currentTarget.classList.add('dragover');
+  });
+
+  column.addEventListener('dragleave', (event) => {
+    event.currentTarget.classList.remove('dragover');
+  });
+
+  column.addEventListener('drop', (event) => {
+    event.preventDefault();
+    event.currentTarget.appendChild(dragged);
+    dragged.classList.remove('dragging');
+    event.currentTarget.classList.remove('dragover');
+  });
+});
+
+allCards.forEach((card) => {
+  card.setAttribute('draggable', 'true');
+
+  card.addEventListener('dragstart', (event) => {
+    dragged = event.target;
+    event.target.classList.add('dragging');
+  });
+
+  card.addEventListener('dragend', (event) => {
+    event.target.classList.remove('dragging');
+    dragged = null;
+  });
+});
