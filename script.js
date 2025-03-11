a2b6b5f13f70e3757738aa19d7f06a4ec96f849d
Pierre Guillard <pguillard95@gmail.com>
2025-03-11 11:46:30 +0100

    add search feature

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-console.log("Kanban JS loaded...");
+console.log('Kanban JS loaded...');
 
 // Exemple éventuel de structure
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // Ici, on récupère les éléments du DOM
-  const addCardBtn = document.getElementById("addCardBtn");
-  const searchInput = document.getElementById("searchInput");
-  const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
+  const addCardBtn = document.getElementById('addCardBtn');
+  const searchInput = document.getElementById('searchInput');
+  const sortByPriorityBtn = document.getElementById('sortByPriorityBtn');
 
   // Éventuellement, on écoute les événements
-  addCardBtn.addEventListener("click", () => {
+  addCardBtn.addEventListener('click', () => {
     // ...
   });
 
-  searchInput.addEventListener("input", () => {
-    // ...
-  });
+  searchInput.addEventListener('input', searchFunction);
 
-  sortByPriorityBtn.addEventListener("click", () => {
+  sortByPriorityBtn.addEventListener('click', () => {
     // ...
   });
 });
