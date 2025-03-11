91cd7ee600e409fbebdcd526c5ce4dd28bb9fc0c
Pierre Guillard <pguillard95@gmail.com>
2025-03-11 15:02:53 +0100

    new cards are draggable

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,32 +1,32 @@
+let dragged = null;
 function enableDragAndDrop() {
-  const allCards = document.querySelectorAll(".card");
-  const columns = document.querySelectorAll(".column");
-
-  let dragged = null;
+  const allCards = document.querySelectorAll('.card');
+  const columns = document.querySelectorAll('.column');
 
   columns.forEach((column) => {
-    column.classList.add("dropzone");
+    column.classList.add('dropzone');
 
-    column.addEventListener("dragover", (event) => {
+    column.addEventListener('dragover', (event) => {
       event.preventDefault();
     });
 
-    column.addEventListener("dragenter", (event) => {
-      event.currentTarget.classList.add("dragover");
+    column.addEventListener('dragenter', (event) => {
+      event.currentTarget.classList.add('dragover');
     });
 
-    column.addEventListener("dragleave", (event) => {
-      event.currentTarget.classList.remove("dragover");
+    column.addEventListener('dragleave', (event) => {
+      event.currentTarget.classList.remove('dragover');
     });
 
-    column.addEventListener("drop", (event) => {
+    column.addEventListener('drop', (event) => {
       if (dragged) {
         event.preventDefault();
         event.currentTarget.appendChild(dragged);
-        dragged.classList.remove("dragging");
-        event.currentTarget.classList.remove("dragover");
-        const newStatus = event.currentTarget.getAttribute("data-status");
-        dragged.setAttribute("data-status", newStatus);
+        dragged.classList.remove('dragging');
+        event.currentTarget.classList.remove('dragover');
+        const newStatus = event.currentTarget.getAttribute('data-status');
+        dragged.setAttribute('data-status', newStatus);
+        console.log(dragged);
 
         saveBoardState();
         dragged = null;
@@ -34,21 +34,23 @@ function enableDragAndDrop() {
     });
   });
 
-allCards.forEach((card) => {
-  addEventDraggable(card);
-});
+  allCards.forEach((card) => {
+    addEventDraggable(card);
+  });
 
-function addEventDraggable(card){
-  card.setAttribute('draggable', 'true');
+  function addEventDraggable(card) {
+    card.setAttribute('draggable', 'true');
 
-    card.addEventListener("dragstart", (event) => {
+    card.addEventListener('dragstart', (event) => {
       dragged = event.target;
-      event.target.classList.add("dragging");
+      console.log(dragged);
+
+      event.target.classList.add('dragging');
     });
 
-    card.addEventListener("dragend", (event) => {
-      event.target.classList.remove("dragging");
+    card.addEventListener('dragend', (event) => {
+      event.target.classList.remove('dragging');
       dragged = null;
     });
-  };
+  }
 }
