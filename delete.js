ecf28eb4022f3c4948a2c3e74b2015cd75a475ca
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-03-11 12:12:03 +0100

    Delete card

--- delete.js
+++ delete.js
@@ -1,23 +1,21 @@
-
-
 window.addEventListener("DOMContentLoaded", () => {
-const card = document.getElementsByClassName("card");
-const close = document.getElementById("close");
-// const card = document.getElementById("card")
+// const card = document.getElementsByClassName("card");
+// const close = document.getElementById("close");
+const card = document.querySelector(".card");
 var closeButton = document.createElement("button");
-close.appendChild(closeButton);
+card.appendChild(closeButton);
 closeButton.innerHTML="X";
-close.addEventListener("click", () => {
-     close.parentNode.remove();
+card.addEventListener("click", () => {
+     card.remove();
     // card.remove();
   });
 
 
-console.log(card.length)
-for (let i = 0; i < card.length; i++) {
+// console.log(card.length)
+// for (let i = 0; i < card.length; i++) {
     
-    console.log(card.length) 
-}
+//     console.log(card.length) 
+// }
 // card.remove();
 
 });
